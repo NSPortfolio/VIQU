dcec1fa3513c86d593ad40489b81ec830b80d8c2
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-12-17 14:46:20 -0600

    Update app.py

--- data/app.py
+++ data/app.py
@@ -1,21 +1,41 @@
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 import plotly.express as px
 import pandas as pd
 
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 df = pd.read_csv('data_merged.csv')
 
 fig = px.line(df, x="date", y="sales", title='Pink Morsel Sales Over Time')
-fig.show()
 
 app.layout = html.Div([
-    dcc.Graph(
-        id='pink-morsel-sales',
-        figure=fig
-    )
+    html.Div([
+        dcc.RadioItems(
+            ['North', 'East', 'South', 'West', 'All'],
+            'All',
+                id='radio-button-value',
+                inline=True
+            )
+        ], style={'width': '48%', 'display': 'inline-block', 'textAlign': 'center'}),
+    dcc.Graph(id='graph-with-radio-buttons'),
 ])
+
+@app.callback(
+    Output('graph-with-radio-buttons', 'figure'),
+    Input('radio-button-value', 'value'))
+    
+def update_graph(radio_button_value):
+    if radio_button_value == 'All':
+        dff = df
+    else:
+        dff = df[df['region'] == radio_button_value.lower()]
+
+    fig = px.line(dff, x="date", y="sales", title='Pink Morsel Sales Over Time')
+
+    return fig
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
