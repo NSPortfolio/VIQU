f6bd71b41c4451150ac9c95a079f37d12209b6af
Nehal S <76495035+NSPortfolio@users.noreply.github.com>
2022-12-17 18:05:18 -0600

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -5,6 +5,6 @@ from app import app
 def test_app(dash_duo):
     dash_duo.start_server(app)
     dash_duo.wait_for_element("#header", timeout=10)
-    dash_duo.wait_for_element("#visualization", timeout=10)
-    dash_duo.wait_for_element("#region_picker", timeout=10)
+    dash_duo.wait_for_element("#radio_buttons", timeout=10)
+    dash_duo.wait_for_element("#figure", timeout=10)
     
